d3d6ea492992e3e498318dc0fd68ba44268405c6
Niz46 <paujohn66@gmail.com>
2025-08-01 09:02:16 +0100

    setup commit

--- blackstone/settings.py
+++ blackstone/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -120,6 +121,7 @@ AUTH_USER_MODEL = "users.User"
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = (os.path.join(BASE_DIR / "static"),)
 
 MEDIA_URL = "/media/"
